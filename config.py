118cba4e8da052a37fabdc78716beff275c28e3c
Anikunshshiw <87267097+Anikunshshiw@users.noreply.github.com>
2021-07-11 18:51:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("6103295"))
+API_HASH = os.getenv("b9c4c0673ca409caffb2ae162e82ddfb")
+BOT_TOKEN = os.getenv("1695161700:AAGLWAjONfgLnBHjojYXv8T5jyogyeheqiA")
 DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+OWNER_ID = list({int(x) for x in os.environ.get("" https://t.me/hsongplaybot"").split()})
